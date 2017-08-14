a33a590e6775f57e26ccaca1dd7a73212fc01621
Ignacio Suay <ignacio.suay@gmail.com>
2017-08-14 13:24:49 +0100

    create edge

---
+++ src/main/java/com/ignaciosuay/datastructure/graph/Edge.java
@@ -0,0 +1,17 @@
+package com.ignaciosuay.datastructure.graph;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Edge<E> {
+    private E element;
+    private Vertex start;
+    private Vertex end;
+    private double weight;
+}
